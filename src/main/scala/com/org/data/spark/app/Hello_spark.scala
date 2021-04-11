722370ec64ebced791798f9049fc03da767d9639
Ananda Ayyasamy <ananda.ayyasamy@aciworldwide.com>
2021-04-11 18:55:18 -0400

    Spark docker build

--- src/main/scala/com/org/data/spark/app/Hello_spark.scala
+++ src/main/scala/com/org/data/spark/app/Hello_spark.scala
@@ -16,27 +16,20 @@ package com.org.data.spark.app
 import org.apache.spark.sql.SparkSession
 
 
-
-
-
 object Hello_spark {
-    // run it in windows
-    System.setProperty("hadoop.home.dir", "C:\\software\\hadoop");
 
-    def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
       .builder()
       .appName("Welcome to Spark 2.+++")
-      .master("local[*]")
       .getOrCreate();
 
     import spark.implicits._
 
-      Seq(("Honda","accord",2012),("Honda","civic",2009)).toDF("Make","Model","Year").show(false)
-
-
-
+    Seq(("Honda", "accord", 2012), ("Honda", "civic", 2009))
+      .toDF("Make", "Model", "Year")
+      .show(false)
 
 
   }
